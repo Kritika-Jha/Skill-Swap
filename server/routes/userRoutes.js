9600f1c4838ae5f34cafd1cea73558138c1f6188
Kritika Jha <kritikajha247@gmail.com>
2025-02-14 22:31:51 +0530

    userRoute

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -29,7 +29,8 @@ router.get("/:userId", async (req, res) => {
 router.post("/add-skill", async (req, res) => {
   try {
     const { userId, skill } = req.body;
-    if (!mongoose.Types.ObjectId.isValid(userId)) return res.status(400).json({ error: "Invalid user ID format" });
+    if (!mongoose.Types.ObjectId.isValid(userId)) 
+      return res.status(400).json({ error: "Invalid user ID format" });
 
     const user = await User.findById(userId);
     if (!user) return res.status(404).json({ error: "User not found" });
@@ -45,6 +46,7 @@ router.post("/add-skill", async (req, res) => {
     res.status(500).json({ error: "Server Error" });
   }
 });
+
 
 // ✅ Update Skill Rating After Quiz
 router.post("/update-skill", async (req, res) => {
