cee25c3368a4094663a90236b8009d4568ba5303
Kritika Jha <kritikajha247@gmail.com>
2025-02-16 09:58:20 +0530

    Fixed rating issue

--- src/components/QuizPage.js
+++ src/components/QuizPage.js
@@ -102,7 +102,7 @@ const QuizPage = () => {
       
       console.log(rating); // Output the rating
 
-      await axios.post("https://skill-swap-u2xd.onrender.com/api/user/update-skill", {
+      await axios.post("http://localhost:5000/api/user/update-skill", {
         userId,
         skillName,
         rating,
